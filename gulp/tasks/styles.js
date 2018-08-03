f694420ada80f26b63c420ee479f37950ea72ea6
Lidiia <lidiiany92@gmail.com>
2018-08-03 17:50:58 -0400

    Testing responsive images

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,14 +3,20 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'), //adds prefixes for us to the items that need them 
 cssvars = require('postcss-simple-vars'), //uses vars values
 nested = require('postcss-nested'),//we can nest our custom css file
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 /* We want to move contents of this file to a pipe so we can do something 
 We also create a pipe where we will take our stylesheet through PostCss filters.
+We are creating .on method which will handle when the error occures watch task will not stop running.
 */
 
 gulp.task('styles', function(){
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+        .on('error', function(errorInfo){
+            console.log(errorInfo.toString());
+            this.emit('end');
+        })
         .pipe(gulp.dest('./app/temp/styles/'));
 });
